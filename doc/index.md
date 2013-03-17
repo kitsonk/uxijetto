08c07342e1a31ea5ea0a335367d703d289812ccb
Kitson Kelly <dojo@kitsonkelly.com>
2013-03-17 14:15:53 +0000

    Initial port of code from DojoX trunk

---
+++ doc/index.md
@@ -0,0 +1,9 @@
+# uxijetto
+
+The `uxijetto` package contains extra Dijit based widgets that were originally part of DojoX, plus a few more widgets
+of which might be useful.
+
+## Former DojoX Widgets
+
+
+## "New" Widgets
--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-define([], function () {
-
-	/** @module dojo2-boilerplate-package/main */
-
-	/**
-		The main function
-		@param {mixed} arg - An argument
-	*/
-	function main(arg) {
-		console.log(arg);
-	}
-
-	return main;
-});
+define([], 1);
